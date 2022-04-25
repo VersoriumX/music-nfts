7f158c4093316951f9ad8f5e0ba0fbb8248b0e12
Nicholas Nikas <nicholasnikas@Nicholass-MacBook-Pro.local>
2022-04-25 15:01:54 -0400

    Implement smart contract and deploy script

---
+++ src/backend/contracts/MusicNFTMarketplace.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MusicNFTMarketplace is ERC721("DAppFi","DAPP"), Ownable {
+    string public baseURI =
+        "https://bafybeifq7jpttu7be5okdtfita6m4tor5xd5wppvmwbul77jsvm763vy3y.ipfs.nftstorage.link/";
+
+    string public baseExtension = ".json";
+    address public artist;
+    uint256 public royaltyFee;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        uint256 price;
+    }
+    MarketItem[] public marketItems;
+
+    constructor(
+        uint256 _royaltyFee,
+        address _artist,
+        uint256[] memory _prices
+    ) payable {
+        require(
+            _prices.length * _royaltyFee <= msg.value,
+            "Deployer must pay royalty fee for each token listed on the marketplace"
+        );
+        royaltyFee = _royaltyFee;
+        artist = _artist;
+        for (uint8 i = 0; i < _prices.length; i++) {
+            require(_prices[i] > 0, "Price must be greater than 0");
+            _mint(address(this), i);
+            marketItems.push(MarketItem(i, payable(msg.sender), _prices[i]));
+        }
+    }
+}
